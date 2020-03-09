e2537061c97ffeaca62f1e031099e518e04cf053
Pedro Semeano <semeano@gmail.com>
2020-03-09 15:14:19 +0000

    Move email address to .env file.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const https = require('https')
 const sgMail = require('@sendgrid/mail')
 
 require('dotenv').config()
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const {SENDGRID_API_KEY, FROM, TO} = process.env
+sgMail.setApiKey(SENDGRID_API_KEY)
 
 
 https.get('https://timeular.com/jobs/', (res) => {
@@ -11,13 +13,13 @@ https.get('https://timeular.com/jobs/', (res) => {
     let match = s.match(/<title>.+<\/title>/)
     if (!!match) {
       let title = s.substring(match.index + 7, match.index + match[0].length - 19)
-      if (title.toLowerCase() !== "page not found") {        
+      if (title.toLowerCase() == "page not found") {        
         const msg = {
-          to: 'semeano@gmail.com',
-          from: 'semeano.me <server@semeano.me>',
+          to: TO,
+          from: FROM,
           subject: 'Timeular has new jobs!',
-          text: 'Check new jobs available at Timeular: https://timeular.com/jobs/',
-          html: 'Check new jobs available at <a href="https://timeular.com/jobs/">Timeular!</a>'
+          text: 'New jobs available at Timeular: https://timeular.com/jobs/',
+          html: 'New jobs available at <a href="https://timeular.com/jobs/">Timeular</a>!'
         }
 
         sgMail
